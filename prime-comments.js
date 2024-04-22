d90efcda76912012cdb8ce8d10f1ea7a2f305965
Sidarth Erat <sidarth.erat@gmail.com>
2024-04-22 07:01:20 -0700

    added some comments to the code

--- prime-comments.js
+++ prime-comments.js
@@ -1,15 +1,27 @@
+/*
+gdata stores a list of colors representing whether or not a number is prime
+specifically, if gdata.colors[i] == "green" then i+1 has not been classified yet
+if gdata.colors[i] == "red" then i+1 is a composite number
+if gdata.colors[i] == "orange" then i+1 is a prime number
+*/
+
 var gdata;
 
+/*
+canvas and context are (constant) variables initialized to draw 
+to the canvas
+*/
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+//drawStuff uses gdata.colors to render the canvas
 function drawStuff() {
    let x = 0,
       y = 0;
-
+   // sets width of squares
    let width = canvas.width / 30;
    let pad = 2;
-
+   // fills the canvas with rows of evenly spaced squares labelled with numbers
    for (let i = 0; i < gdata.colors.length; i++) {
       context.fillStyle = gdata.colors[i];
       context.fillRect(x, y, width, width);
@@ -27,23 +39,40 @@ function drawStuff() {
    }
 }
 
+/*
+ every time a color change is needed, it is appended to pending_changes
+ */
 var pending_changes = [];
 
+/*
+the variable speed controls the rate at which pending_changes are applied
+*/
 var speed = 50;
 
+// callback function checkKey is set up when any key is pressed
 document.onkeydown = checkKey;
+
+/*
+checkKey speeds up the rate at which pending_changes are applied if 
+right arrow is pressed, and slows down the rate if left arrow is pressed
+*/
 
 function checkKey(e) {
    e = e || window.event;
 
    if (e.keyCode == "37") {
-
+      // left arrow
       speed = 2 * speed;
    } else if (e.keyCode == "39") {
-
+      // right arrow
       speed = speed / 2;
    }
 }
+
+/* 
+display_changes applies the first element of pending_changes and 
+sets a timeout between changes
+*/
 
 function display_changes() {
    var tmp = pending_changes.shift();
@@ -55,6 +84,16 @@ function display_changes() {
    context.fillText(tmp.text, tmp.txtx, tmp.txty);
    setTimeout(display_changes, speed);
 }
+
+/* 
+  begin_animation initializes the animation by:
+ (1) retrieving the maximum value of the sieve
+ (2) initializing gdata.colors to all green except the first element 
+     (since 1 is not prime)
+ (3) calculating the height needed for the canvas
+ (4) calling drawStuff to create the grid, starting the sieve, and 
+     setting the initial timeout period before pending_updates are applied
+*/
 
 function begin_animation() {
    let maxN = Number(document.getElementById("maxN").value) - 1;
@@ -68,6 +107,7 @@ function begin_animation() {
    };
    gdata.colors.unshift("red");
    canvas.width = window.innerWidth;
+   //  canvas.height = window.innerHeight;
 
    let width = canvas.width / 30;
    let pad = 2;
@@ -76,16 +116,22 @@ function begin_animation() {
    y += width + pad;
    canvas.height = y;
 
+
    drawStuff();
    start();
    setTimeout(display_changes, speed);
 }
 
+/*
+changes the color of n by calculating the position of n in the grid and 
+adding it to pending_updates
+*/
 function change_color(n, ncolor) {
    let x = 0,
       y = 0;
    let width = canvas.width / 30;
    let pad = 2;
+
 
    n = n - 1;
    let numrow = Math.floor((canvas.width - pad) / (width + pad))
@@ -102,6 +148,13 @@ function change_color(n, ncolor) {
    });
 }
 
+//BEGINNING OF SIEVE IMPLEMENTATION
+
+/*
+checks for the next smallest number that has not been proven composite 
+(i.e., marked green)
+*/
+
 function get_next_prime() {
    for (let i = 0; i < gdata.colors.length; i++) {
       if (gdata.colors[i] == "green") {
@@ -111,6 +164,10 @@ function get_next_prime() {
    return -1;
 }
 
+/*
+marks primes as orange and composites as red. Composites are marked red 
+by iterating through multiples of primes (sieve of Eratosthenes)
+*/
 function iterate(prime) {
    gdata.colors[prime - 1] = "orange";
    change_color(prime, "orange");
@@ -120,10 +177,19 @@ function iterate(prime) {
    }
 }
 
+/*
+this function allows for generalization to a simulation for multiple 
+sieves. It is not important to this code and may be ignored.
+*/
 function start() {
    start_sieve1();
 }
 
+/*
+this function implements the sieve by repeatedly getting the next prime 
+and marking its multiples as composite through get_next_prime() and 
+iterate().
+*/
 function start_sieve1() {
    var prime = 2;
    while (prime > 0) {
